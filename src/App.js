a8e2cb05763a332e1e56a11a41056b0128f8f9e3
deneme <deneme@denemes-MacBook-Air.local>
2018-03-01 18:20:57 +0300

    first commit 2

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+
+
+      <div className="App">
+
+          <Navbar>
+              <Navbar.Header>
+                  <Navbar.Brand>
+                      <a href="#home">React-Bootstrap</a>
+                  </Navbar.Brand>
+              </Navbar.Header>
+              <Nav>
+                  <NavItem eventKey={1} href="#">
+                      Link
+                  </NavItem>
+                  <NavItem eventKey={2} href="#">
+                      Link
+                  </NavItem>
+                  <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                      <MenuItem eventKey={3.1}>Action</MenuItem>
+                      <MenuItem eventKey={3.2}>Another action</MenuItem>
+                      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                      <MenuItem divider />
+                      <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                  </NavDropdown>
+              </Nav>
+          </Navbar>;
+
+        <header className="App-header">
+          <h1 className="App-title">Tarla Pazar</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
