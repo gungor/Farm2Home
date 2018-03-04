a9e71f2107b1f23a5f024c13e365b7bd0c4df51f
deneme <deneme@denemes-MacBook-Air.local>
2018-03-04 18:44:37 +0300

    Pagination Customization 1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Navbar, Nav, NavItem} from 'react-bootstrap/lib/';
 import DataList from './components/DataList'
 
 
-
 class App extends Component {
     render() {
         return (
@@ -13,44 +12,46 @@ class App extends Component {
 
             <div className="App">
 
-                <div className="menu-container">
-                    <Navbar>
-                        <Nav>
-                            <NavItem eventKey={1} href="#">
-                                Sat
-                            </NavItem>
-                            <NavItem eventKey={2} href="#">
-                                Ürünlerim
-                            </NavItem>
-                            <NavItem eventKey={3} href="#">
-                                Mesaj
-                            </NavItem>
-                            <NavItem eventKey={4} href="#">
-                                Ürün İsteyenler
-                            </NavItem>
-                        </Nav>
+                <div className="container-fluid navbar-font">
+                    <div className="row justify-content-md-center">
+                        <div className="col-sm-10">
+                            <Navbar>
+                                <Navbar.Toggle/>
+                                <Navbar.Collapse>
+                                    <Nav>
+                                        <NavItem eventKey={1} href="#">
+                                            Sat
+                                        </NavItem>
+                                        <NavItem eventKey={2} href="#">
+                                            Ürünlerim
+                                        </NavItem>
+                                        <NavItem eventKey={3} href="#">
+                                            Mesajlar
+                                        </NavItem>
+                                        <NavItem eventKey={4} href="#">
+                                            Ürün İsteyenler
+                                        </NavItem>
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Navbar>
+                        </div>
+                        <div className="col-sm-2">
+                            <div className="text-right logout" >
+                                <span>User</span> | <a href='#'>Çıkış</a>
 
-                    </Navbar>
-
-                    <div className="logout">
-
-                        <span>User</span> | <a href='#'>Çıkış</a>
-
+                            </div>
+                        </div>
                     </div>
-                    <div className="clearFloat"></div>
                 </div>
 
                 <header className="App-header">
                     <h1 className="App-title">...</h1>
                 </header>
 
-                <div className='page-header' >En Yeniler</div>
+                <div className='site_page-header'>En Yeni Ürünler</div>
                 <div className='table-container'>
                     <DataList></DataList>
                 </div>
-
-
-
 
 
             </div>
--- src/components/DataList.js
+++ src/components/DataList.js
@@ -1,23 +1,151 @@
-import React, {Component} from 'react'
-import ReactTable from 'react-table'
+import React, {Component} from 'react';
+import ReactTable from 'react-table';
+require('react-table/react-table.css');
 
 
 class DataList extends Component {
     render() {
         const data = [{
-            name: 'dddffsd',
-            min : 5 ,
-            price: 7.5
-        }];
+            name: 'Domates',
+            min: 5,
+            price: 7.5,
+            city: 'Çanakkale'
+        },
+            {
+                name: 'Portakal',
+                min: 5,
+                price: 5,
+                city: 'Antalya'
+            },
+            {
+                name: 'Portakal',
+                min: 10,
+                price: 6,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            },
+            {
+                name: 'Muz',
+                min: 8,
+                price: 3,
+                city: 'Mersin'
+            }];
 
         const columns = [{
             Header: 'Ürün',
             accessor: 'name', // String-based value accessors!
-            Cell: props => <span >{props.value}</span>
+            Cell: props => <span>{props.value}</span>
         },
-            { Header: 'Minimum Sipariş', accessor: 'min' },{Header: 'Kg Fiyat', accessor: 'price'}];
+            {Header: 'Minimum Sipariş (kg)', accessor: 'min'}, {Header: 'Kg Fiyat', accessor: 'price'},{Header: 'Şehir', accessor: 'city'}];
 
-        return <ReactTable  data={data} columns={columns}/>
+        return <ReactTable data={data} columns={columns}  minRows={0} showPageSizeOptions={false} showPageJump={false} defaultPageSize={10}/>
     }
 }
 
